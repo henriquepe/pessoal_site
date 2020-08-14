cd530f92331e0cdf479aeb0413f9b4e0adb49f8b
henriquepe <henrique.p.espindola@gmail.com>
2020-08-14 15:30:37 -0300

    upload build

---
+++ build/precache-manifest.6a1d529f0e4bb8a2e03979bcc927644a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde6f1a06c2b87c9f0c800126fd16976",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f0bed75a376c66fb5d",
+    "url": "/static/js/2.0c4e85e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0c4e85e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8e3ee00cb868fcad82",
+    "url": "/static/js/main.91876b0b.chunk.js"
+  },
+  {
+    "revision": "e0c5a4a5e778671927e0",
+    "url": "/static/js/runtime-main.01bf120f.js"
+  },
+  {
+    "revision": "dc43ad683905ef58864eb631c882341c",
+    "url": "/static/media/code-image.dc43ad68.svg"
+  },
+  {
+    "revision": "f60ba1730e16f78f94d57378fec125c6",
+    "url": "/static/media/contact-guy.f60ba173.png"
+  },
+  {
+    "revision": "b2b45dc613832c3f48a4dbf077f329ff",
+    "url": "/static/media/cssIcon.b2b45dc6.svg"
+  },
+  {
+    "revision": "25fac0ba4ef12d4faf8132b4e634c9cc",
+    "url": "/static/media/dockerIcon.25fac0ba.svg"
+  },
+  {
+    "revision": "a7c5469d6f1ecb33f4c9cd290cab7009",
+    "url": "/static/media/github-2.a7c5469d.svg"
+  },
+  {
+    "revision": "e14e74ba52f12aea0c432a55111832c5",
+    "url": "/static/media/instagram-2.e14e74ba.svg"
+  },
+  {
+    "revision": "861853a0963448cbd445d652e87fd75b",
+    "url": "/static/media/javascriptIcon.861853a0.svg"
+  },
+  {
+    "revision": "0a4a3957487bed339946234fae339155",
+    "url": "/static/media/linkedin-2.0a4a3957.svg"
+  },
+  {
+    "revision": "682f21cc80c0d357065caa42091995dc",
+    "url": "/static/media/nodeIcon.682f21cc.svg"
+  },
+  {
+    "revision": "f9238a1e741ee3afc8b58830742328ba",
+    "url": "/static/media/perfil.f9238a1e.svg"
+  },
+  {
+    "revision": "76e4fa2849d1be8af15107239aa1db27",
+    "url": "/static/media/reactIcon.76e4fa28.svg"
+  },
+  {
+    "revision": "b091155b43f9faa51fdebd8033ea9f91",
+    "url": "/static/media/tag.b091155b.svg"
+  }
+]);
